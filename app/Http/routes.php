112cabaf7f3743a7159f4e743c1094c7ba26b150
zualex32 <Azubarev32@yandex.ru>
2015-09-01 08:45:00 +0300

    Create simple admin with https://scotch.io/tutorials/simple-laravel-crud-with-resource-controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,7 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+
+Route::resource('nerds', 'NerdController');
--- config/app.php
+++ config/app.php
@@ -144,6 +144,11 @@ return [
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
+		
+		/*
+		* Extra
+		*/
+		'Illuminate\Html\HtmlServiceProvider',
 
 	],
 
@@ -192,6 +197,12 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		
+		/*
+		* Extra
+		*/
+		'Form'=> 'Illuminate\Html\FormFacade', 
+		'HTML'=> 'Illuminate\Html\HtmlFacade',
 
 	],
 
