4d258b2ca45026c930c0f2580271e31794a05958
zualex32 <Azubarev32@yandex.ru>
2015-10-02 13:35:00 +0300

    add migrate

---
+++ database/migrations/2015_10_02_133101_create_playlist_extra_videos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaylistExtraVideosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('playlist_extra_videos', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('path');
+			$table->integer('time')->default(0);
+			$table->integer('sort')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('playlist_extra_videos');
+	}
+
+}
