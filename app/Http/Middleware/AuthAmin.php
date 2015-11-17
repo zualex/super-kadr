8c8339d6d3001db99b420b6ac994ae3afd8aeb95
zualex32 <Azubarev32@yandex.ru>
2015-11-17 17:51:57 +0300

    change if

--- app/Http/Middleware/AuthAmin.php
+++ app/Http/Middleware/AuthAmin.php
@@ -1,28 +1,24 @@
 <?php namespace App\Http\Middleware;
-
 use Closure;
 use Auth;
-
 class AuthAmin {
-	
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
 
-	
+	/**
+	* Handle an incoming request.
+	*
+	* @param \Illuminate\Http\Request $request
+	* @param \Closure $next
+	* @return mixed
+	*/
+
 	public function handle($request, Closure $next)
 	{
+		if (Auth::check()){
+			if (Auth::user()->level == 'admin' || Auth::user()->level == 'moderator'){
+				return $next($request);
+			}
+		}
+		return redirect()->guest('auth/login');
 
-		if (Auth::check() && Auth::user()->level == 'admin' || Auth::user()->level == 'moderator'){
-			return $next($request);
-		}else{
-			return redirect()->guest('auth/login');
-		}		
-		
 	}
-
 }
